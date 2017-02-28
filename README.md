7ee4d9a6f7f29b0bc4ce561b907a98427d6d12f9
Thineskumar <Thineskumar.thilakanathan@qvalia.com>
2017-02-28 19:52:44 +0100

    some cleanup

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# AdClientLibrary 
+# AdClientLibrary
 
 Webpack based javascript library for random ads publishing
 
 ## Scripts
 
 * `npm run build` - produces production version of your library under the `lib` folder
+* `npm run server` - running the server
 * `npm run dev` - produces development version of your library and runs a watcher
 * `npm run test` - well ... it runs the tests
 * `npm run test:watch` - same as above but in a watch mode
--- test/adClientLibrary.spec.js
+++ test/adClientLibrary.spec.js
@@ -16,16 +16,15 @@ describe('Given instance of library',  () => {
         var expectvalue = 'adOne';
         it('should return the containerId', () => {
           expect(expectvalue).to.be.equal(containerId);
-          //expect('adTwo').to.be.equal('adTwo');
-          //expect('adThree').to.be.equal('adThree');
-          //expect(60).to.be.equal(60);
-    });
+
+        });
       describe('runAd', () => {
-      var expectUpdateInterval = 60;
-      it('should return the updateInterval', () => {
+        var expectUpdateInterval = 60;
+        it('should return the updateInterval', () => {
         expect(expectUpdateInterval).to.be.equal(updateInterval);
-        
-  });
-  });
-});
+
+        });
+      });
+
+    });
 });
